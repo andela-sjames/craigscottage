0cc81080c56392146608196856bb2ae5fcd453c7
andela-sjames <samuel.james@andela.com>
2018-09-09 02:31:54 +0100

    bootstrap begins

--- cottage/cottage/urls.py
+++ cottage/cottage/urls.py
@@ -7,6 +7,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+from home import views
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
@@ -19,11 +20,13 @@ urlpatterns = [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
+    # url(r'', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
+    url(r'^pages/', include(wagtail_urls)),
+    url(r'^$', views.HomeView.as_view(), name='cakehomepage'),
+
 ]
 
 

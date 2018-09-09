89377387c21d11f068709afa8112ad75b272744b
andela-sjames <samuel.james@andela.com>
2018-09-09 17:32:25 +0100

    seperation of concern

--- cottage/home/views.py
+++ cottage/home/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 class HomeView(TemplateView):
     template_name = 'home/index.html'
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+    def get(self, request):
+        args = {}
+        args['page_title'] = "Craig's - cottage| Home"
+
+        return render(request, self.template_name, args)

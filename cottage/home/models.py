cade7756cf6411f9300353d7345d820f3211d1ee
andela-sjames <samuel.james@andela.com>
2018-09-27 01:02:59 +0100

    update sidebar

--- cottage/home/models.py
+++ cottage/home/models.py
@@ -13,7 +13,7 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 def validate_image(fieldfile_obj):
             filesize = fieldfile_obj.file.size
-            megabyte_limit = 4.0
+            megabyte_limit = 1.0
             if filesize > megabyte_limit*1024*1024:
                 raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
 
